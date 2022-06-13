3e1a003404845e96eac0649f0df07f4b38f50558
Szymofcion <szymon.dziewa1@wp.pl>
2022-06-13 19:23:13 +0200

    repair

--- script.js
+++ script.js
@@ -2,11 +2,11 @@ let todoInput;
 let errorInfo;
 let addBtn;
 let ulList;
-let newTask
+let newTask;
 
 const main = () => {
-  prepereDOMEvents;
-  prepareDOMElements;
+  prepareDOMElements();
+  prepereDOMEvents();
 };
 const prepareDOMElements = () => {
   todoInput = document.querySelector(".todo-input");
@@ -14,42 +14,46 @@ const prepareDOMElements = () => {
   addBtn = document.querySelector(".btn-add");
   ulList = document.querySelector(".todoList ul");
 };
+
+const prepereDOMEvents = () => {
+  addBtn.addEventListener("click", addNewTask);
+};
 const addNewTask = () => {
   if (todoInput.value !== "") {
-    newTask = document.createElement('li')
-    newTask.textContent = todoInput.value
-    ulList.append(newTask)
-
-    todoInput.value = ''
-    errorInfo.textContent = ''
+    newTask = document.createElement("li");
+    newTask.textContent = todoInput.value;
+    createToolsArea();
+    
+    ulList.appendChild(newTask);
+    
+    todoInput.value = "";
+    errorInfo.textContent = "";
   } else {
     errorInfo.textContent = "Wpisz treść zadania!";
   }
 };
 
-const prepereDOMEvents = () => {
-  addBtn.addEventListener("click", addNewTask);
+const createToolsArea = () => {
+  
+  const toolsPanel = document.createElement("div");
+  toolsPanel.classList.add("tools");
+  newTask.appendChild(toolsPanel);
+  
+  const completeBtn = document.createElement("button");
+  completeBtn.classList.add("complete");
+  completeBtn.innerHTML = '<i class="fas fa-check"></i>';
+  
+  const editBtn = document.createElement("button");
+  editBtn.classList.add("edit");
+  editBtn.textContent = "EDIT";
+  
+  const deleteBtn = document.createElement("button");
+  deleteBtn.classList.add("delete");
+  deleteBtn.innerHTML = '<i class="fas fa-times"></i>';
+  
+  toolsPanel.appendChild(completeBtn, editBtn, deleteBtn);
 };
 
-const createToolsArea = () => {
-  const toolsPanel = document.createElement('div')
-  toolsPanel.classList.add('tools')
-  newTask.append(toolsPanel)
-
-  const completeBtn = document.createElement('button')
-  commpleteBtn.classList.add('complete')
-  commpleteBtn.innerHTML = '<i class="fas fa-check"></i>'
-
-  const editBtn = document.createElement('button')
-  editBtn.classList.add('edit')
-  editBtn.textContent = 'EDIT'
-
-  const deleteBtn = document.createElement('button')
-  deleteBtn.classList.add ('delete')
-  commpleteBtn.innerHTML = '<i class="fas fa-times"></i>'
-
-  toolsPanel.append(completeBtn, editBtn, deleteBtn)
-}
+document.addEventListener("DOMContentLoaded", main);
 
 console.log(addBtn);
-document.addEventListener('DOMContentLoaded', main)
