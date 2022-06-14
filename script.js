0728acd5d6bee3d10c2ab55964330b9e72b6f2bf
Szymofcion <szymon.dziewa1@wp.pl>
2022-06-14 19:39:31 +0200

    feat: button popup accept and edit

--- script.js
+++ script.js
@@ -4,6 +4,13 @@ let addBtn;
 let ulList;
 let newTask;
 
+let popup;
+let popupInfo;
+let todoToEdit;
+let popupInput;
+let popupAddBtn;
+let popupCloseBtn;
+
 const main = () => {
   prepareDOMElements();
   prepereDOMEvents();
@@ -12,12 +19,20 @@ const prepareDOMElements = () => {
   todoInput = document.querySelector(".todo-input");
   errorInfo = document.querySelector(".error-info");
   addBtn = document.querySelector(".btn-add");
-  ulList = document.querySelector(".todoList ul");
-  
+  ulList = document.querySelector(".todolist ul");
+
+  popup = document.querySelector(".popup");
+  popupInfo = document.querySelector(".popup-info");
+  popupInput = document.querySelector(".popup-input");
+  popupAddBtn = document.querySelector(".accept");
+  popupCloseBtn = document.querySelector(".cancel");
 };
 
 const prepereDOMEvents = () => {
   addBtn.addEventListener("click", addNewTask);
+  ulList.addEventListener("click", checkClick);
+  popupCloseBtn.addEventListener("click", closePopup);
+  popupAddBtn.addEventListener('click', changeTodoText)
 };
 const addNewTask = () => {
   if (todoInput.value !== "") {
@@ -27,7 +42,7 @@ const addNewTask = () => {
 
     console.log(ulList);
     ulList.appendChild(newTask);
-    
+
     todoInput.value = "";
     errorInfo.textContent = "";
   } else {
@@ -36,24 +51,51 @@ const addNewTask = () => {
 };
 
 const createToolsArea = () => {
-
   const toolsPanel = document.createElement("div");
   toolsPanel.classList.add("tools");
   newTask.appendChild(toolsPanel);
-  
+
   const completeBtn = document.createElement("button");
   completeBtn.classList.add("complete");
   completeBtn.innerHTML = '<i class="fas fa-check"></i>';
-  
+
   const editBtn = document.createElement("button");
   editBtn.classList.add("edit");
   editBtn.textContent = "EDIT";
-  
+
   const deleteBtn = document.createElement("button");
   deleteBtn.classList.add("delete");
   deleteBtn.innerHTML = '<i class="fas fa-times"></i>';
-  
+
   toolsPanel.append(completeBtn, editBtn, deleteBtn);
+};
+
+const checkClick = (e) => {
+  if (e.target.matches(".complete")) {
+    e.target.closest("li").classList.toggle("completed");
+
+    e.target.classList.toggle("completed");
+  } else if (e.target.matches(".edit")) {
+    editTodo(e);
+  } else if (e.target.matches(".delete")) {
+  }
+};
+
+const editTodo = (e) => {
+  todoToEdit = e.target.closest("li");
+
+  popupInput.value = todoToEdit.firstChild.textContent;
+  popup.style.display = "flex";
+};
+
+const closePopup = () => {
+  popup.style.display = "none";
+};
+
+const changeTodoText = () => {
+  if (popupInput.value !== "") {
+    todoToEdit.firstChild.textContent = popupInput.value;
+  }
 };
 
 document.addEventListener("DOMContentLoaded", main);
